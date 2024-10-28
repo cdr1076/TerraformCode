755bafcdf0914e5d2c4e641243820793321ab1ec
cdr1076 <cdr1076@hotmail.com>
2024-10-28 18:27:46 +0000

    terraform and docker

---
+++ docker-tf/main.tf
@@ -0,0 +1,30 @@
+# Chris Rusev
+
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Specifying Docker image for container
+resource "docker_image" "nginx" {
+  name         = "nginx:1.19.6"
+  keep_locally = true    // keep image after "destroy"
+}
+
+# Docker Container build info
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 2224
+  }
+}
+
